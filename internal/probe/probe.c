e3b268fa6207ae2c661243654336756e2bc9d36b
Patrick Gaskin <patrick@pgaskin.net>
2026-05-26 22:41:18 -0400

    Make probe code indentation consistent

--- internal/probe/probe.c
+++ internal/probe/probe.c
@@ -14,14 +14,14 @@
 #define UNTAG(p) ((void *)((__u64)(p) & ~(0xFFUL << 56)))
 
 struct config {
-    __s64 s3;            // offsetof(bssl::SSL, s3)
+	__s64 s3;            // offsetof(bssl::SSL, s3)
 	__s64 client_random; // offsetof(bssl::SSL3_STATE, client_random)
 };
 
 struct event {
-    __s64 debug_line;
-    __s64 debug_ret;
-    __s64 debug_ptr;
+	__s64 debug_line;
+	__s64 debug_ret;
+	__s64 debug_ptr;
 	char  label[LABEL_MAX];
 	__u8  client_random[CLIENT_RANDOM_SIZE];
 	__u8  secret[SECRET_MAX];
@@ -54,58 +54,58 @@ int uprobe_ssl_log_secret(struct pt_regs *ctx) {
 
 	struct event ev = {};
 	if (!ssl || !label || !secret) {
-        ev.debug_line = __LINE__;
-        goto emit;
-    }
+		ev.debug_line = __LINE__;
+		goto emit;
+	}
 
 	__u32 key = 0;
 	struct config *cfg = bpf_map_lookup_elem(&config_map, &key);
 	if (!cfg) {
-        ev.debug_line = __LINE__;
-        goto emit;
-    }
+		ev.debug_line = __LINE__;
+		goto emit;
+	}
 
 	if ((ev.debug_ret = bpf_probe_read_user_str(ev.label, sizeof(ev.label), label)) < 0) { // label is in .rodata, no tagging
-        ev.debug_ptr = (__s64) label;
-        ev.debug_line = __LINE__;
-        goto emit;
-    }
+		ev.debug_ptr = (__s64) label;
+		ev.debug_line = __LINE__;
+		goto emit;
+	}
 
 	ev.secret_len = secret_len;
 
 	if (secret_len < 0) {
 		secret_len = 0;
-    }
+	}
 	if (secret_len > SECRET_MAX) {
 		secret_len = SECRET_MAX;
-    }
+	}
 	if (secret_len > 0 && (ev.debug_ret = bpf_probe_read_user(ev.secret, secret_len, UNTAG(secret))) < 0) {
-        ev.debug_ptr = (__s64) secret;
-        ev.debug_line = __LINE__;
-        goto emit;
-    }
+		ev.debug_ptr = (__s64) secret;
+		ev.debug_line = __LINE__;
+		goto emit;
+	}
 
 	void *s3 = NULL;
-    if ((ev.debug_ret = bpf_probe_read_user(&s3, sizeof(s3), (__u8 *)UNTAG(ssl) + cfg->s3)) < 0) {
-        ev.debug_ptr = (__s64) (__u8 *)ssl + cfg->s3;
-        ev.debug_line = __LINE__;
-        goto emit;
-    }
-    if (s3 == NULL) {
-        ev.debug_line = __LINE__;
-        goto emit;
-    }
-    if ((ev.debug_ret = bpf_probe_read_user(ev.client_random, sizeof(ev.client_random), (__u8 *)UNTAG(s3) + cfg->client_random)) < 0) {
-        ev.debug_ptr = (__s64) (__u8 *)s3 + cfg->client_random;
-        ev.debug_line = __LINE__;
-        goto emit;
-    }
+	if ((ev.debug_ret = bpf_probe_read_user(&s3, sizeof(s3), (__u8 *)UNTAG(ssl) + cfg->s3)) < 0) {
+		ev.debug_ptr = (__s64) (__u8 *)ssl + cfg->s3;
+		ev.debug_line = __LINE__;
+		goto emit;
+	}
+	if (s3 == NULL) {
+		ev.debug_line = __LINE__;
+		goto emit;
+	}
+	if ((ev.debug_ret = bpf_probe_read_user(ev.client_random, sizeof(ev.client_random), (__u8 *)UNTAG(s3) + cfg->client_random)) < 0) {
+		ev.debug_ptr = (__s64) (__u8 *)s3 + cfg->client_random;
+		ev.debug_line = __LINE__;
+		goto emit;
+	}
 
-    ev.debug_ret = 0;
-    ev.debug_ptr = 0;
-    ev.debug_line = -1; // ok
-    emit:
-    bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &ev, sizeof(ev));
+	ev.debug_ret = 0;
+	ev.debug_ptr = 0;
+	ev.debug_line = -1; // ok
+	emit:
+	bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &ev, sizeof(ev));
 	return 0;
 }
 
